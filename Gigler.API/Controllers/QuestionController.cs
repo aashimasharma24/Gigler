12c6682757c800761db271630704eaf8195d5210
Aashima Sharma <aashimasharma10@gmail.com>
2024-10-29 13:39:04 +0530

    Added Core project. Added question controller

--- Gigler.API/Controllers/QuestionController.cs
+++ Gigler.API/Controllers/QuestionController.cs
@@ -4,16 +4,16 @@ namespace Gigler.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class QuestionController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<QuestionController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public QuestionController(ILogger<QuestionController> logger)
         {
             _logger = logger;
         }
